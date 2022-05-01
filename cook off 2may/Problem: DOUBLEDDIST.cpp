746ff1d2ab343f91bba2a6c3f49d9c437fbb3c7d
scopophobic <66241061+scopophobic@users.noreply.github.com>
2022-05-02 03:45:25 +0530

    Create Problem: DOUBLEDDIST.cpp

---
+++ cook off 2may/Problem: DOUBLEDDIST.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    bool f=true;
+	    sort(arr,arr+n);
+	    for(int i=1;i<n-1;i++){
+	        int x=arr[i]-arr[i-1];
+	        int y=arr[i+1]-arr[i];
+	        if(2*x!=y && x!=2*y){
+	            f=false;
+	            break;
+	        }
+	    }
+	    if(f){
+	        cout<<"YES"<<endl;
+	        
+	    }
+	    
+	    else{
+	        cout<<"NO"<<endl;
+	    } 
+	}
+	
+	return 0;
+}
