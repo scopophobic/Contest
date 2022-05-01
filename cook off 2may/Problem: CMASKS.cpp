8c8021ec1b584efdc259d437a5d4b0222fc05393
scopophobic <66241061+scopophobic@users.noreply.github.com>
2022-05-02 03:42:52 +0530

    Create Problem: CMASKS.cpp

---
+++ cook off 2may/Problem: CMASKS.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/COOK141D/problems/CMASKS
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x*100<y*10){
+	        cout<<"Disposable"<<endl;
+	    }
+	    else{
+	        cout<<"Cloth"<<endl;
+	    }
+	}
+	return 0;
+}
