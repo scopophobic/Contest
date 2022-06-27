eb91338b37f9140631fc2e21cf85f323880ffdaf
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-06-28 01:14:04 +0530

    Create Problem: FIT.cpp

---
+++ long_june_codechef/Problem: FIT.cpp
@@ -0,0 +1,16 @@
+// Problem: FIT Contest: JUNE222D 
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	   int n;
+	   cin>>n;
+	   cout<<n*10<<endl;
+	}
+	return 0;
+}
