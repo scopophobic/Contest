ce794ae2fd65aa14fa9c95d3049109922d5335aa
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-07-13 21:50:04 +0530

    Create Problem Code:CHESSTIME.cpp

---
+++ Codechef_sol/Starters_47/Problem Code:CHESSTIME.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int h=n*60;
+	    int ans=h/20;
+	    cout<<ans<<endl;
+	}
+	
+	return 0;
+}
