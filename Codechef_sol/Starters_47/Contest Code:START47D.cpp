926f35571351090d458fd1a01c7f4d5a1fd47861
Adithyan Madhu <66241061+scopophobic@users.noreply.github.com>
2022-07-13 21:49:27 +0530

    Create Contest Code:START47D.cpp

---
+++ Codechef_sol/Starters_47/Contest Code:START47D.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b;
+	    cin>>a>>b;
+	    if(a>=b){
+	        cout<<"Yes"<<endl;
+	    }
+	    else{
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
